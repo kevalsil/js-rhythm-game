b4511027a5b7f0517dc4c109d6b0c3695f4eeb9e
KEVAL <koreaminsoo@naver.com>
2024-09-15 22:48:32 +0900

    Update result.js

--- js/result.js
+++ js/result.js
@@ -88,7 +88,7 @@ function updateRatingDisplay(rate) {
 
 // 랭킹 불러오기
 function fetchRankings() {
-    fetch(`http://api.kevalsil.com/rankings?song=${encodeURIComponent(song)}`)
+    fetch(`https://api.kevalsil.com/rankings?song=${encodeURIComponent(song)}`)
         .then(response => response.json())
         .then(data => {
             let rankingHtml = '';
@@ -128,7 +128,7 @@ function sendScoreToServer(song, player, score, rate) {
         return;
     }
 
-    fetch('http://api.kevalsil.com/submit-score', {
+    fetch('https://api.kevalsil.com/submit-score', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
